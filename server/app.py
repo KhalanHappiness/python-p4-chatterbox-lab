1d95f1d1b56af5d6a1dff574cdcfc42b84372896
happiness <happinesskhalan@gmail.com>
2025-06-20 00:48:19 +0300

    chatterbox challenge complete

--- server/app.py
+++ server/app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+from flask_cors import CORS
+
 
 CORS(app)
 migrate = Migrate(app, db)
@@ -16,11 +18,72 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+
+    messages = Message.query.all()
+
+    messages_dict = [message.to_dict() for message in messages]
+
+    response = make_response(
+        messages_dict,
+        200,
+        {"Content-Type": "application/json"}
+
+    )
+    return response
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if message:
+        message_dict = message.to_dict()
+
+        response = make_response(
+        message_dict,
+        200,
+        )
+        return response
+
+    return jsonify({"error": "id not found"})
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+
+    new_msg = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+
+    db.session.add(new_msg)
+    db.session.commit()
+
+    return jsonify(new_msg.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    msg = Message.query.get(id)
+
+    if not msg:
+        return jsonify({"error": "Message not found"}), 404
+
+    data = request.get_json()
+    msg.body = data.get('body', msg.body)
+    db.session.commit()
+
+    return jsonify(msg.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    msg = Message.query.get(id)
+
+    if not msg:
+        return jsonify({"error": "Message not found"}), 404
+
+    db.session.delete(msg)
+    db.session.commit()
+    return jsonify({}), 204
+
 
 if __name__ == '__main__':
     app.run(port=5555)
